6ab1f2534751f788014c21081773db7e68d065f1
Jacinto <jacinto.ramirez@corvoconsultores.com>
2020-07-08 20:50:23 -0700

    docs agregado

---
+++ docs/precache-manifest.4786b34c7f9884a70798e5635d680f12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8e9a94ee6513057e4868ca965b29d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e096b6155baf40725e4d",
+    "url": "/static/css/main.2cf6a771.chunk.css"
+  },
+  {
+    "revision": "3747f9e238cb47a667cf",
+    "url": "/static/js/2.24e3cec1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.24e3cec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e096b6155baf40725e4d",
+    "url": "/static/js/main.38bfde8d.chunk.js"
+  },
+  {
+    "revision": "c74e8f71fcaa354e7f22",
+    "url": "/static/js/runtime-main.cffaa9ef.js"
+  }
+]);
